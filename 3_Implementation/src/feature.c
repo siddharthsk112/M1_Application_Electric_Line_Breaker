4447cce72561cd0caf9037afe45f15a8ab3929e0
siddharthsk112 <68293365+siddharthsk112@users.noreply.github.com>
2021-11-25 09:18:36 +0530

    Update feature.c

--- 3_Implementation/src/feature.c
+++ 3_Implementation/src/feature.c
@@ -2,24 +2,49 @@
 #include<string.h>
 int passwords[4]={1234, 2341, 3412, 4123};
 int line_state1[4] = {1, 0, 0, 0};
+/**
+ * @brief Get the Input Choice object
+ * 
+ * @param choice 
+ * @return int 
+ */
 int getInputChoice(int choice)
 {
     if(choice==0 || choice==1)
         return choice;
     return -1;
 }
+/**
+ * @brief Get the Line Number object
+ * 
+ * @param line_no 
+ * @return int 
+ */
 int getLineNumber(int line_no)
 {
     if(line_no>0 && line_no<5)
         return line_no;
     return 0;
 }
+/**
+ * @brief the check the password matching
+ * 
+ * @param password 
+ * @param line 
+ * @return int 
+ */
 int is_password_matching(int password, int line)
 {
     if(password== passwords[line-1])
         return 1;
     return 0;
 }
+/**
+ * @brief Get the Line Operation object
+ * 
+ * @param ch 
+ * @return int 
+ */
 int getLineOperation(int ch)
 {
 
@@ -27,6 +52,12 @@ int getLineOperation(int ch)
         return ch;
     return 0;
 }
+/**
+ * @brief Get the Line State object
+ * 
+ * @param line 
+ * @return int 
+ */
 int getLineState(int line)
 {
     return line_state1[line-1];
