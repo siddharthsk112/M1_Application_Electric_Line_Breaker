af362736beff984b3ad7b276384dad281fe1ab9f
siddharthsk112 <68293365+siddharthsk112@users.noreply.github.com>
2021-11-25 07:37:04 +0530

    Create feature.h

---
+++ 3_Implementation/inc/feature.h
@@ -0,0 +1,18 @@
+/**
+ * @file feature.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef FEATURE_H_
+#define FEATURE_H_
+int getInputChoice(int choice);
+int getLineNumber(int line_no);
+int is_password_matching(int password, int line);
+int getLineOperation(int ch);
+int getLineState(int line);
+#endif
