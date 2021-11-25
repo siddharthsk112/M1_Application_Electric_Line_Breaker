422bb1f37afca3ed6673fb324b32324c547fb01f
siddharthsk112 <68293365+siddharthsk112@users.noreply.github.com>
2021-11-25 09:17:35 +0530

    Update feature.h

--- 3_Implementation/inc/feature.h
+++ 3_Implementation/inc/feature.h
@@ -1,7 +1,7 @@
 /**
  * @file feature.h
- * @author your name (you@domain.com)
- * @brief 
+ * @author Siddharth (you@domain.com)
+ * @brief this is the header file for the project(ELECTRIC_LINE_BREAKER)
  * @version 0.1
  * @date 2021-11-24
  * 
@@ -10,9 +10,40 @@
  */
 #ifndef FEATURE_H_
 #define FEATURE_H_
+/**
+ * @brief Get the Input Choice object
+ * 
+ * @param choice 
+ * @return int 
+ */
 int getInputChoice(int choice);
+/**
+ * @brief Get the Line Number object
+ * 
+ * @param line_no 
+ * @return int 
+ */
 int getLineNumber(int line_no);
+/**
+ * @brief 
+ * 
+ * @param password 
+ * @param line 
+ * @return int 
+ */
 int is_password_matching(int password, int line);
+/**
+ * @brief Get the Line Operation object
+ * 
+ * @param ch 
+ * @return int 
+ */
 int getLineOperation(int ch);
+/**
+ * @brief Get the Line State object
+ * 
+ * @param line 
+ * @return int 
+ */
 int getLineState(int line);
 #endif
